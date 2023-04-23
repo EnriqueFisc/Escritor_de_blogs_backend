7b404bd77e372f1b1bb8881d9dc949e706024a16
EnriqueFisc <enriquefisde@gmail.com>
2023-04-23 14:46:39 -0700

    actualizacion de base de datos y variables de entorno

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.get( '*', ( req, res ) => {
     res.sendFile( pagePath );
 });
 
-app.listen( process.env.PORT, () => {
-    console.log( `Server on port: ${ process.env.PORT }` );
+app.listen( process.env.PORT || 5000, () => {
+    console.log( `Server on port: ${ process.env.PORT || 5000 }` );
 });
